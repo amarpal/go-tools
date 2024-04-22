c4803655e2ccbb4f6b536ea97fa5f708e132d4ac
Amarpal Singh <amarpal@goheadlands.com>
2024-04-21 20:48:59 -0700

    SA1101: reports usage of log.Fatal in packages not named 'main'

--- staticcheck/analysis.go
+++ staticcheck/analysis.go
@@ -36,6 +36,7 @@ import (
 	"github.com/amarpal/go-tools/staticcheck/sa1031"
 	"github.com/amarpal/go-tools/staticcheck/sa1032"
 	"github.com/amarpal/go-tools/staticcheck/sa1100"
+	"github.com/amarpal/go-tools/staticcheck/sa1101"
 	"github.com/amarpal/go-tools/staticcheck/sa2000"
 	"github.com/amarpal/go-tools/staticcheck/sa2001"
 	"github.com/amarpal/go-tools/staticcheck/sa2002"
@@ -135,6 +136,7 @@ var Analyzers = []*lint.Analyzer{
 	sa1031.SCAnalyzer,
 	sa1032.SCAnalyzer,
 	sa1100.SCAnalyzer,
+	sa1101.SCAnalyzer,
 	sa2000.SCAnalyzer,
 	sa2001.SCAnalyzer,
 	sa2002.SCAnalyzer,
